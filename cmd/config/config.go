754d0a8ed82e2464e02799f69c6c8530f00eec9f
Luke <luchenqun@qq.com>
2024-01-26 23:05:53 +0800

    feat(app): change account prefix to ethos and base denom to aethos

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -24,7 +24,7 @@ import (
 
 const (
 	// Bech32Prefix defines the Bech32 prefix used for EthAccounts
-	Bech32Prefix = "evmos"
+	Bech32Prefix = "ethos"
 
 	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
 	Bech32PrefixAccAddr = Bech32Prefix
@@ -42,9 +42,9 @@ const (
 
 const (
 	// DisplayDenom defines the denomination displayed to users in client applications.
-	DisplayDenom = "evmos"
-	// BaseDenom defines to the default denomination used in Evmos (staking, EVM, governance, etc.)
-	BaseDenom = "aevmos"
+	DisplayDenom = "ethos"
+	// BaseDenom defines to the default denomination used in Ethos (staking, EVM, governance, etc.)
+	BaseDenom = "aethos"
 )
 
 // SetBech32Prefixes sets the global prefixes to be used when serializing addresses and public keys to Bech32 strings.
--- x/erc20/keeper/ibc_callbacks_test.go
+++ x/erc20/keeper/ibc_callbacks_test.go
@@ -371,7 +371,7 @@ func (suite *KeeperTestSuite) TestOnRecvPacket() {
 }
 
 func (suite *KeeperTestSuite) TestConvertCoinToERC20FromPacket() {
-	senderAddr := "evmos1x2w87cvt5mqjncav4lxy8yfreynn273xn5335v"
+	senderAddr := "ethos1x2w87cvt5mqjncav4lxy8yfreynn273x4svxvj"
 
 	testCases := []struct {
 		name     string
@@ -590,7 +590,7 @@ func (suite *KeeperTestSuite) TestOnAcknowledgementPacket() {
 }
 
 func (suite *KeeperTestSuite) TestOnTimeoutPacket() {
-	senderAddr := "evmos1x2w87cvt5mqjncav4lxy8yfreynn273xn5335v"
+	senderAddr := "ethos1x2w87cvt5mqjncav4lxy8yfreynn273x4svxvj"
 
 	testCases := []struct {
 		name     string
